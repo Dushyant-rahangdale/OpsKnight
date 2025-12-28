1ea82bc07d59b68ea4a1b025c140b0e34a12ac53
Dushyant Rahangdale <dushyant.rahangdale@axicorp.com>
2025-12-28 20:10:18 +0530

    feat: secure deployment workflow with dynamic SG authorization

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,7 +4,7 @@ output "public_ip" {
   sensitive   = true
 }
 
-output "ssh_command" {
-  description = "Command to SSH into your server"
-  value       = "ssh -i path/to/${var.ssh_key_name}.pem ec2-user@${aws_eip.web_eip.public_ip}"
+output "security_group_id" {
+  description = "The ID of the security group, needed for GitHub Actions to authorize itself."
+  value       = aws_security_group.web_sg.id
 }
