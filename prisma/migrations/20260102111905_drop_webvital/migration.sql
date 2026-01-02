bb4931ca98c822b7febb3e6234843e97eaa2ff31
Dushyant Rahangdale <dushyant.rahangdale@axi.com>
2026-01-03 04:48:32 +0530

    fix(prisma): split migration to separate enum alteration from table drops

--- prisma/migrations/20260102111905_drop_webvital/migration.sql
+++ prisma/migrations/20260102111905_drop_webvital/migration.sql
@@ -4,7 +4,8 @@
   - You are about to drop the `WebVital` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- AlterEnum
-ALTER TYPE "IncidentUrgency" ADD VALUE 'MEDIUM';
+-- DropForeignKey
+ALTER TABLE "WebVital" DROP CONSTRAINT "WebVital_userId_fkey";
 
-
+-- DropTable
+DROP TABLE "WebVital";
